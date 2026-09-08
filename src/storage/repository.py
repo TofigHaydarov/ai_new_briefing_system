5101ce57afd3ccec1e46400623bc74b51a9c3513
Haji708 <ilhamhaciyev126@gmail.com>
2026-09-08 09:36:59 +0400

    follow-up on the last commit

--- src/storage/repository.py
+++ src/storage/repository.py
@@ -10,7 +10,7 @@ class UserProfile(BaseModel):
     max_items_per_topic: int
     @classmethod
     def from_dict(cls,data:dict):
-        return cls(data.get("user"),'',data.get("preferred_topics",[]),data.get("excluded_sources",[]),data.get("max_items_per_topic",3))
+        return cls(data.get("user"),data.get("preferred_topics",[]),data.get("excluded_sources",[]),data.get("max_items_per_topic",3))
         
 
     
