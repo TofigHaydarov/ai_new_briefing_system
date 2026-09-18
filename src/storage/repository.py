007f2e06f72948e0f6d07943d9fc073bf618a7d4
Haji708 <ilhamhaciyev126@gmail.com>
2026-09-18 16:53:31 +0400

    added Exception to save_profile in repository.py to avoid empty profiles

--- src/storage/repository.py
+++ src/storage/repository.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 from typing import List, Optional
 from pydantic import BaseModel, Field
-
+import asyncio
 class UserProfile(BaseModel):
     user: str
     preferred_topics: List[str] = Field(default_factory=list)
@@ -11,6 +11,8 @@ class UserProfile(BaseModel):
     
     @classmethod
     def from_dict(cls,data:dict):
+        if data=={}:
+            raise ValueError("Empty data provided for UserProfile")
         return cls(user = data.get("user",''), preferred_topics = data.get("preferred_topics",[]), excluded_sources = data.get("excluded_sources",[]), max_items_per_topic = data.get("max_items_per_topic",3))
     def __str__(self):
         return f"Name:{self.user}\n Preferred Topics:{self.preferred_topics}\n Excluded sources:{self.excluded_sources}\n"
@@ -54,6 +56,8 @@ class JSONUserRepo:
         return UserProfile.from_dict(user_data)
     
     async def save_profile(self,profile:UserProfile):
+        if profile.user =='':
+            raise ValueError("User name cannot be empty")
         with open(self.file_path,'r') as f:
             data = json.load(f)
         if "user" in data and isinstance(data["user"], str):
@@ -62,9 +66,6 @@ class JSONUserRepo:
         data[profile.user] = profile.model_dump(by_alias=True)
         with open(self.file_path, "w") as f:
             json.dump(data, f,indent = 4)
-"""
-userProf = UserProfile(user = "haji" ,preferred_topics= ["Football"],excluded_sources= ["Jews"] ,max_items_per_topic=5)
-
-userRepo = JSONUserRepo(Path("C:/Users/Haji/Desktop/SWE Project/ai_new_briefing_system/data/user_profile.json"))
-userRepo.save_profile(userProf)
-"""
+test_repo = JSONUserRepo(Path(__file__).parent.parent / "data" / "user_profile.json")
+test_profile = UserProfile(user="", preferred_topics=[], excluded_sources=[], max_items_per_topic=3)
+asyncio.run(test_repo.save_profile(test_profile))
